265cb8ccb33ef076a33a768906308b6b511619dd
Aditya Itnal <161662309+winnand@users.noreply.github.com>
2025-12-25 23:17:23 -0600

    Added Firmware files

---
+++ Firmware/winnand_tkl/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x53, 0x7D, 0xA4, 0xFC, 0x81, 0x95, 0xB4}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 } //Use Shift-esc as the unlock
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
